4581987b13a915b4630156f877f3b05475d9530f
VŨ AN THỌ <113340239+thovuan@users.noreply.github.com>
2024-02-05 16:00:48 +0700

    Update HELWOR.cs

--- HELWOR.cs
+++ HELWOR.cs
@@ -3,7 +3,7 @@ namespace INITIAL {
     class HELWOR {
         public static void Main(string[] args) {
             Console.WriteLine("Hello World");
-            Console.WriteLine("Hai mu 0 = 1 hihi");
+            Console.WriteLine("Hai mu 0 = 0");
         }
     }
 }
